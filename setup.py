6d30f6affd2a6cf3df56d92dd69f61566817e5a2
Brian Cline <brian.cline@gmail.com>
2013-11-13 03:42:40 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ceilometer-riemann',
-    version='0.2.0',
+    version='0.2.1',
     author='Brian Cline',
     author_email='bcline@softlayer.com',
     description=('Riemann publisher driver for OpenStack Ceilometer'),
